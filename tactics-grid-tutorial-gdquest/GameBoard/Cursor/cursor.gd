85ae6e6e3be36bb8b3743af70eb1cffedc4604bb
pjz987 <pwj2012@gmail.com>
2023-08-27 09:25:52 -0700

    i fuckin hate this

--- tactics-grid-tutorial-gdquest/GameBoard/Cursor/cursor.gd
+++ tactics-grid-tutorial-gdquest/GameBoard/Cursor/cursor.gd
@@ -22,7 +22,22 @@ signal moved(new_cell)
 
 # Coordinates of the current cell the cursor is hovering.
 var cell := Vector2.ZERO:
-	set = set_cell
+	set(value):
+		print('what the fuckin fuckin fuck!!!!!?!?!??!?!?!?')
+		# We first clamp the cell coordinates and ensure that we aren't
+		#	trying to move outside the grid boundaries
+		var new_cell: Vector2 = grid.grid_clamp(value)
+		if new_cell.is_equal_approx(cell):
+			return
+
+		cell = new_cell
+		# If we move to a new cell, we update the cursor's position, emit
+		#	a signal, and start the cooldown timer that will limit the rate
+		#	at which the cursor moves when we keep the direction key held
+		#	down
+		position = grid.calculate_map_position(cell)
+		emit_signal("moved", cell)
+		_timer.start()
 
 # We use the timer to have a cooldown on the cursor movement.
 @onready var _timer: Timer = $Timer
